6ad9bdf51351110664707a3f09423f4d617170b2
Haferm98 <105403350+Haferm98@users.noreply.github.com>
2024-10-20 13:14:04 -0400

    Create Book class with properties and methods

--- library_management.js
+++ library_management.js
@@ -1,1 +1,32 @@
+//Task 1: Create a 'Book' Class
 
+class Book
+{
+    #_isAvailable;
+    constructor(tile, author, ISBN)
+    {
+        this.title = title;
+        this.author = author;
+        this.ISBN = ISBN;
+        this.#_isAvailable = true;
+    }
+
+// Retrieve Details
+getDetails() 
+{
+    return `Book: ${this.title}, Author: ${this.author}, ISBN: ${this.ISBN}`;
+};
+
+//Check Availibility
+get _isAvailable()
+{
+    return this.#_isAvailable;
+};
+
+//Assign Availablility
+
+set isAvailable(availability)
+{
+    this.#_isAvailable = avialability;
+}
+};
