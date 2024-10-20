b8e167b26e6ac0b2ec0fea6d8c580863c0491121
Haferm98 <105403350+Haferm98@users.noreply.github.com>
2024-10-20 13:53:46 -0400

    Create Patron class to borrow and return books

--- library_management.js
+++ library_management.js
@@ -65,3 +65,33 @@ listBooks()
 };
 
 };
+
+//Task 3: Create a Patron Class
+class Patron
+{
+    constructor(name, borrowedBooks)
+    {
+        this.name = name;
+        this.borrowedBooks = [];
+    }
+
+//Borrow a Book
+borrowBook(book)
+{
+    if (book._isAvailable)
+    {
+        this.borrowedBooks.push(book);
+        book._isAvailable = false;
+    };
+};
+
+
+//Return a Book
+
+returnBook(book)
+{
+    const updateBooks = this.borrowedBooks.filter((borrowedBook) +. borrowedBook === book)
+    this.borrowedBooks = updateBooks;
+    book._isAvailable = true;
+};
+};
